7ae6ca6acf7410ed6521bbf6b286d5eb84577b0c
Adam Homel <adam.d.homel@marquette.edu>
2023-11-29 17:44:26 -0600

    Add project files.

---
+++ Project_INSY4051/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Project_INSY4051
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
